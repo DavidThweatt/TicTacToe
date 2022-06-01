3d47d5aafa89eb9c5fc49df6276cc49b56bf251c
David Thweatt <68977760+DavidThweatt@users.noreply.github.com>
2022-06-01 10:08:17 -0700

    code clean up
    code clean up and theme change

--- script.js
+++ script.js
@@ -19,15 +19,7 @@ const creatPlayer = (name, marker) => {
   const squares = document.getElementsByClassName("squares");
   const restart = document.getElementById("restart");
 
-  // create players
-  twoPlayer.addEventListener("click", () => {
-    const Player1 = creatPlayer("1", "X");
-    const Player2 = creatPlayer("2", "O");
-    Display.innerHTML = "X's turn!";
-  });
-
   // player turns
-
   let nextTurn = "X";
 
   function changeTurn() {
@@ -71,49 +63,49 @@ const creatPlayer = (name, marker) => {
     if (
       (s1.innerHTML === "X" || s1.innerHTML === "O") &&
       s1.innerHTML === s2.innerHTML &&
-      s1.innerHTML === s3.innerHTML
+      s3.innerHTML
     ) {
       gameOver(s1.innerHTML);
     } else if (
       (s4.innerHTML === "X" || s4.innerHTML === "O") &&
       s4.innerHTML === s5.innerHTML &&
-      s4.innerHTML === s6.innerHTML
+      s6.innerHTML
     ) {
       gameOver(s4.innerHTML);
     } else if (
       (s7.innerHTML === "X" || s7.innerHTML === "O") &&
       s7.innerHTML === s8.innerHTML &&
-      s7.innerHTML === s9.innerHTML
+      s9.innerHTML
     ) {
       gameOver(s7.innerHTML);
     } else if (
       (s1.innerHTML === "X" || s1.innerHTML === "O") &&
       s1.innerHTML === s4.innerHTML &&
-      s1.innerHTML === s7.innerHTML
+      s7.innerHTML
     ) {
       gameOver(s1.innerHTML);
     } else if (
       (s2.innerHTML === "X" || s2.innerHTML === "O") &&
       s2.innerHTML === s5.innerHTML &&
-      s2.innerHTML === s8.innerHTML
+      s8.innerHTML
     ) {
       gameOver(s2.innerHTML);
     } else if (
       (s3.innerHTML === "X" || s3.innerHTML === "O") &&
       s3.innerHTML === s6.innerHTML &&
-      s3.innerHTML === s9.innerHTML
+      s9.innerHTML
     ) {
       gameOver(s3.innerHTML);
     } else if (
       (s1.innerHTML === "X" || s1.innerHTML === "O") &&
       s1.innerHTML === s5.innerHTML &&
-      s1.innerHTML === s9.innerHTML
+      s9.innerHTML
     ) {
       gameOver(s1.innerHTML);
     } else if (
       (s3.innerHTML === "X" || s3.innerHTML === "O") &&
       s3.innerHTML === s5.innerHTML &&
-      s3.innerHTML === s7.innerHTML
+      s7.innerHTML
     ) {
       gameOver(s3.innerHTML);
     }
@@ -128,19 +120,4 @@ const creatPlayer = (name, marker) => {
       window.location.reload();
     }
   }
-
-  //    [s1,s2,s3]
-  //    [s4,s5,s6]
-  //    [s7,s8,s9]
-
-  //    [s1,s4,s7]
-  //    [s2,s5,s8]
-  //    [s3,s6,s9]
-
-  //    [s1,s5,s9]
-  //    [s3,s5,s7]
-
-  // squares.removeEventListener('click', () => {
-
-  // })
 })();
